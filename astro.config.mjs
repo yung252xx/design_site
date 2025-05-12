b28111c3d295bcba6c63f9870480b58f86256747
nathanxx <wakkoa349@gmail.com>
2025-05-12 04:55:45 -0400

    updated gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 
 export default defineConfig({
   output: "static",
   base: "/design_site/",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
