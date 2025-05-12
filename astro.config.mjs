1227a2067b502f37668acd039401f3b331d66b3a
nathanxx <wakkoa349@gmail.com>
2025-05-12 04:44:35 -0400

    updated gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  output: "static",
+  base: "/design_site/",
   vite: {
     plugins: [tailwindcss()]
   }
---
+++ src/data/galleryData.js
@@ -0,0 +1,12 @@
+export const galleryImage = [
+    {src:'/assets/images/LOVEISLAW.png', title: 'Love is Law'},
+    {src:'/assets/images/design_shit.png', title: 'design shit'},
+    {src:'/assets/images/', title: ''},
+    {src:'/assets/images/', title: ''},
+    {src:'/assets/images/', title: ''},
+    {src:'/assets/images/', title: ''},
+    {src:'/assets/images/', title: ''},
+    {src:'/assets/images/', title: ''},
+    {src:'/assets/images/', title: ''},
+    {src:'/assets/images/', title: ''},
+];
